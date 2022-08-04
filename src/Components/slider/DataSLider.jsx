f525a9e709946b29bbc1da8ad862a28f1f38e966
Akinsuyiphilip <philipakinbolade@gmail.com>
2022-08-04 02:53:25 +0100

    files

--- src/Components/slider/DataSLider.jsx
+++ src/Components/slider/DataSLider.jsx
@@ -3,7 +3,7 @@
         
         title:"Astro Craft - Keep tracing and capture venture",
         Sub: "Augmation reality solution with latest technology enhances real estate.",
-        img: './imageslider/Img.jpg',
+        img: './imageslider/Img.png',
     },
     {
         
